9346769f65d33487a1d79488c33f2080f96a370b
Juanicus <juanicus.bravo@gmail.com>
2022-03-15 13:17:16 +0100

    Finializada suma

---
+++ src/Service/TestService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service;
+
+
+class TestService {
+
+    public function metodoDePruebas(): string
+    {
+
+        $hola = 'hola sheep!';
+
+        return $hola;
+    }
+
+    public function suma ($o1, $o2){
+
+        return $o1+$o2;
+
+    }
+
+}
+
